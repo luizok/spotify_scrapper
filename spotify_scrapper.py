59e8be70126ac716a8d9dbcfd1d88c4583117133
luizok <luizfelipesmaciel@gmail.com>
2021-01-03 14:28:30 -0300

    Reorganized imports

--- spotify_scrapper.py
+++ spotify_scrapper.py
@@ -1,11 +1,14 @@
+import re
+from logger import get_logger
+
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
-from logger import get_logger
+from objects import SpotifyMusic
 
 
 log = get_logger(__name__)
